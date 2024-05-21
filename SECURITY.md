3eedb94b969355f20a88223b2899332033c0f026
Honchar Denys <crackeraki@gmail.com>
2024-05-21 19:27:46 +0300

    major cleanup before work on lib it self

--- SECURITY.md
+++
@@ -1,5 +0,0 @@
-# Security Policy
-
-## Reporting a Vulnerability
-
-If you have a found a security issue or vulnerability in this package, please report it to [huntr.dev](https://huntr.dev/bounties/disclose).
--- browser/comments.js
+++
@@ -1,2 +0,0 @@
-/*! Swig v0.0.0-development | https://node-swig.github.com/swig-templates | @license https://github.com/node-swig/swig-templates/blob/master/LICENSE */
-/*! DateZ (c) 2011 Tomo Universalis | @license https://github.com/TomoUniversalis/DateZ/blob/master/LISENCE */
